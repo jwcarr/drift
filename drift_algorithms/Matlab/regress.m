a61d1c6fc09da7c9a2e1774ff6af16a2b71cee6f
Jon Carr <jwcarr@users.noreply.github.com>
2020-05-10 17:40:51 +0200

    Fixed Matlab calls to normcdf and normpdf

--- drift_algorithms/Matlab/regress.m
+++ drift_algorithms/Matlab/regress.m
@@ -23,15 +23,15 @@ function fixation_XY = regress(fixation_XY, line_Y, k_bounds, o_bounds, s_bounds
 	m = length(line_Y);
 
 	function [goodness_of_fit, line_assignments] = fit_lines(params)
-		k = k_bounds(1) + (k_bounds(2) - k_bounds(1)) * cdf('Normal', params(1));
-		o = o_bounds(1) + (o_bounds(2) - o_bounds(1)) * cdf('Normal', params(2));
-		s = s_bounds(1) + (s_bounds(2) - s_bounds(1)) * cdf('Normal', params(3));
+		k = k_bounds(1) + (k_bounds(2) - k_bounds(1)) * normcdf(params(1));
+		o = o_bounds(1) + (o_bounds(2) - o_bounds(1)) * normcdf(params(2));
+		s = s_bounds(1) + (s_bounds(2) - s_bounds(1)) * normcdf(params(3));
 		density = zeros(n, m);
 		predicted_Y_from_slope = fixation_XY(:, 1).' * k;
 		line_Y_plus_offset = line_Y + o;
 		for line_i = 1 : m
 			fit_Y = predicted_Y_from_slope + line_Y_plus_offset(line_i);
-			density(:, line_i) = log(pdf('Normal', fixation_XY(:, 2).', fit_Y, s));
+			density(:, line_i) = log(normpdf(fixation_XY(:, 2).', fit_Y, s));
 		end
 		[max_density, line_assignments] = max(density, [], 2);
 		goodness_of_fit = -sum(max_density);
