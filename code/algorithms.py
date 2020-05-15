643303bce5e0540ff9406a7441debc0014eb6bb3
Jon Carr <jwcarr@users.noreply.github.com>
2020-05-15 19:47:03 +0200

    Added matchup algorithm and correct_drift

--- code/algorithms.py
+++ code/algorithms.py
@@ -3,6 +3,24 @@ from sklearn.cluster import KMeans
 from scipy.optimize import minimize
 from scipy.stats import norm
 
+def correct_drift(method, fixation_XY, passage):
+	if method == 'attach':
+		return attach(fixation_XY, passage.line_positions)
+	elif method == 'chain':
+		return chain(fixation_XY, passage.line_positions)
+	elif method == 'cluster':
+		return cluster(fixation_XY, passage.line_positions)
+	elif method == 'matchup':
+		return matchup(fixation_XY, passage.line_positions, passage.word_centers())
+	elif method == 'regress':
+		return regress(fixation_XY, passage.line_positions)
+	elif method == 'segment':
+		return segment(fixation_XY, passage.line_positions)
+	elif method == 'warp':
+		return warp(fixation_XY, passage.char_xy)
+	else:
+		raise ValueError('Invalid method')
+
 def attach(fixation_XY, line_Y):
 	n = len(fixation_XY)
 	for fixation_i in range(n):
@@ -33,6 +51,21 @@ def cluster(fixation_XY, line_Y):
 	for fixation_i, cluster_i in enumerate(clusters):
 		line_i = np.where(ordered_cluster_indices == cluster_i)[0][0]
 		fixation_XY[fixation_i, 1] = line_Y[line_i]
+	return fixation_XY
+
+def matchup(fixation_XY, line_Y, word_XY, x_thresh=512):
+	n = len(fixation_XY)
+	diff_X = np.diff(fixation_XY[:, 0])
+	end_line_indices = list(np.where(diff_X < -x_thresh)[0] + 1)
+	end_line_indices.append(n)
+	text_lines = [word_XY[np.where(word_XY[:, 1] == line_y)] for line_y in line_Y]
+	start_line_index = 0
+	for end_line_index in end_line_indices:
+		gaze_line = fixation_XY[start_line_index:end_line_index]
+		line_costs = [dynamic_time_warping(gaze_line, text_line)[0] for text_line in text_lines]
+		line_i = np.argmin(line_costs)
+		fixation_XY[start_line_index:end_line_index, 1] = line_Y[line_i]
+		start_line_index = end_line_index
 	return fixation_XY
 
 def regress(fixation_XY, line_Y, k_bounds=(-0.1, 0.1), o_bounds=(-50, 50), s_bounds=(1, 20)):
@@ -73,7 +106,7 @@ def segment(fixation_XY, line_Y):
 	return fixation_XY
 
 def warp(fixation_XY, character_XY):
-	warping_path, cost = dynamic_time_warping(fixation_XY, character_XY)
+	_, warping_path = dynamic_time_warping(fixation_XY, character_XY)
 	for fixation_i, characters_mapped_to_fixation_i in enumerate(warping_path):
 		candidate_Y = character_XY[characters_mapped_to_fixation_i, 1]
 		fixation_XY[fixation_i, 1] = mode(candidate_Y)
@@ -86,25 +119,25 @@ def mode(values):
 def dynamic_time_warping(sequence1, sequence2):
 	n1 = len(sequence1)
 	n2 = len(sequence2)
-	cost_matrix = np.zeros((n1+1, n2+1))
-	cost_matrix[0, :] = np.inf
-	cost_matrix[:, 0] = np.inf
-	cost_matrix[0, 0] = 0
+	dtw_cost = np.zeros((n1+1, n2+1))
+	dtw_cost[0, :] = np.inf
+	dtw_cost[:, 0] = np.inf
+	dtw_cost[0, 0] = 0
 	for i in range(n1):
 		for j in range(n2):
-			cost = np.sqrt((sequence1[i,0] - sequence2[j,0])**2 + (sequence1[i,1] - sequence2[j,1])**2)
-			cost_matrix[i+1, j+1] = cost + min(cost_matrix[i, j+1], cost_matrix[i+1, j], cost_matrix[i, j])
-	cost_matrix = cost_matrix[1:, 1:]
-	warping_path = [[] for _ in range(n1)]
+			this_cost = np.sqrt(sum((sequence1[i] - sequence2[j])**2))
+			dtw_cost[i+1, j+1] = this_cost + min(dtw_cost[i, j+1], dtw_cost[i+1, j], dtw_cost[i, j])
+	dtw_cost = dtw_cost[1:, 1:]
+	dtw_path = [[] for _ in range(n1)]
 	while i > 0 or j > 0:
-		warping_path[i].append(j)
+		dtw_path[i].append(j)
 		possible_moves = [np.inf, np.inf, np.inf]
 		if i > 0 and j > 0:
-			possible_moves[0] = cost_matrix[i-1, j-1]
+			possible_moves[0] = dtw_cost[i-1, j-1]
 		if i > 0:
-			possible_moves[1] = cost_matrix[i-1, j]
+			possible_moves[1] = dtw_cost[i-1, j]
 		if j > 0:
-			possible_moves[2] = cost_matrix[i, j-1]
+			possible_moves[2] = dtw_cost[i, j-1]
 		best_move = np.argmin(possible_moves)
 		if best_move == 0:
 			i -= 1
@@ -113,5 +146,5 @@ def dynamic_time_warping(sequence1, sequence2):
 			i -= 1
 		else:
 			j -= 1
-	warping_path[0].append(0)
-	return warping_path, cost_matrix[-1, -1]
+	dtw_path[0].append(0)
+	return dtw_cost[-1, -1], dtw_path
