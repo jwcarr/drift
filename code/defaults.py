1e9ee5e79fee77a260f5962f74996bf3ee5315ee
Jon Carr <jwcarr@users.noreply.github.com>
2020-05-28 13:23:08 +0200

    Modified algorithm colors to make more distinct

--- code/defaults.py
+++ code/defaults.py
@@ -1,14 +1,14 @@
 y_to_line_mapping = {155:1, 219:2, 283:3, 347:4, 411:5, 475:6, 539:7, 603:8, 667:9, 731:10, 795:11, 859:12, 923:13}
 
-colors = {'attach':'#6B6B7F',
-          'chain':'#E85A71',
-          'cluster':'#4EA1D3',
-          'compare':'#E0782A',
+colors = {'attach':'#81818D',
+          'chain':'#D64E7A',
+          'cluster':'#45ADEC',
+          'compare':'#F3711A',
           'merge':'#205E84',
-          'regress':'#FCBE32',
-          'segment':'#17A363',
-          'split':'#FF644E',
-          'warp':'#7544D6',
+          'regress':'#FCAF32',
+          'segment':'#009142',
+          'split':'#E32823',
+          'warp':'#6742B0',
           'gold':'#B0944B',
           'JC':'black',
           'VP':'black'
